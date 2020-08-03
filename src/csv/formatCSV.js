3fd6266729ee881a57d2c2d8e11eb9410eb54fb9
JoannaSprottHome <joanna@joannas-mbp.mynet>
2020-08-03 20:24:58 +0100

    Add formatArrayByDateTotals

--- src/csv/formatCSV.js
+++ src/csv/formatCSV.js
@@ -25,82 +25,32 @@ const formatArrayByDate = array => {
   return newArr;     
 };
 
-const formatArrayByDateTotals = (array, [propsToConsolidate]) => { 
-  const formattedByDate = formatArrayByDate(array);
-  formattedByDate.forEach(each => { // this is each day
-    const key = Object.keys(each)[0]; // we only have one object per day
-    const value = each[key];
-    console.log(`value: ${
-      JSON.stringify(
-        value
-        )
-    }`);
+/**
+ * Add multiple entries of the same day together
+ * @param {array} array 
+ * @param {properties to add} param1 // TODO this should be optional
+ */
+const formatArrayByDateTotals = (array, [propsToConsolidate]) => { // TODO some refactoring of this function
+  let formattedByDate = formatArrayByDate(array);
+  formattedByDate.forEach(day => {
+    const key = Object.keys(day)[0]; // we only have one object per day
+    const value = day[key];
+    const totals = {};
+    value.forEach(entry => {
+      Object.keys(entry).forEach(prop => {
+        if (propsToConsolidate.includes(prop)) {
+          if (!totals[prop]) {
+            totals[prop] = entry[prop];
+          } else {
+            const currentValue = totals[prop];
+            totals[prop] = Number(totals[prop]) + Number(entry[prop]);
+          }
+        } 
+      });      
+    });
+    day[key] = totals;
   });
-  // Object.keys(formattedByDate).forEach(key => {
-  //   console.log("the key" + key);        
-  //   console.log(formattedByDate[key]); 
-  // });
-
-  
-
-
+  return formattedByDate;
 };
-
-// const formatArrayByDateTotalsORIG = (array, [propsToConsolidate]) => { 
-//   //console.log(propsToConsolidate);
-//   //1 only add the props we want
-//   // 1a iterate over the object keys - DONE
-//   // 1b work out if they are in the array - DONE
-//   // 2 add them to the existing value
-
-//   // [prop1, prop2]
-//   // [{many props}, {}, {}]
-
-//   // iterate over the original array (forEach)
-//   // iterate over each prop 
-
-//   array.forEach((each) => { 
-//     // this is each array with one object
-//     // check if new array is empty
-//     // if so add the first
-//   });
-
-//   let onlyTheProps = [];
-//   array.forEach((each) => { // but this is 3 entries...
-//     onlyTheProps = Object.entries(each).map(eachObj => {
-//       const prop = eachObj[0];
-//       if (propsToConsolidate.includes(prop)) {
-//         if (onlyTheProps.length === 0) {
-//           return {[prop]: each[prop]};
-//         } else {
-//           console.log(onlyTheProps);
-//           console.log("needs to be summed");
-//           return {[prop]: each[prop]};
-//         }
-//       }
-//     }).filter(defined => defined);
-//   });
-
-//   const consolidatedObj = onlyTheProps.reduce(function(result, current) {
-//     return Object.assign(result, current);
-//   }, {});
-
-//   /////////////////////////////////////////////
-//   const newArr = []; 
-//   array.forEach((each) => {
-//     const dateObj = {
-//       [each.Date]: each   
-//     };
-//     const index = datePresent(each.Date, newArr);
-//     if (index < 0) {
-//       newArr.push(dateObj);
-//     } else {      
-//       const key = Object.keys(newArr[index]);
-//       //console.log(newArr[index][key]);
-//       //newArr[index][key].push(each); // instead of pushing, need to sum the totals
-//     }  
-//   });
-//   return newArr;     
-// }; 
 
 export { formatArrayByDate, formatArrayByDateTotals };
